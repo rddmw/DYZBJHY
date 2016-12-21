d311a63dfaf8ba51b3a90e4aa680d02f77232842
rddmw <274534310@qq.com>
2016-12-21 22:49:50 +0800

    添加home模块的nav

---
+++ DYZBJHY/DYZBJHY/HomeViewController.swift
@@ -0,0 +1,33 @@
+//
+//  HomeViewController.swift
+//  DYZBJHY
+//
+//  Created by hugh.jia on 2016/12/21.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+    }
+    
+    
+
+}
+
+
+extension HomeViewController{
+    fileprivate func setupUI() {
+        
+        setupNavUI()
+    }
+    
+    private func setupNavUI() {
+        
+    }
+}
